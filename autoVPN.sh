614d547ed77e21b4b87f96e338fe1588234818f4
svs-devel <example@example.com>
2020-10-29 14:45:29 +0300

    autoVPN script additions

--- autoVPN.sh
+++ autoVPN.sh
@@ -1,7 +1,11 @@
+#!/bin/bash
+let START_TIMEOUT=5
+let CHECK_TIMEOUT=60
 SERVER_IP_ADDR="192.168.100.1"
-VPN_CONNECTION_NAME="test_vpn_connection"
+VPN_CONNECTION_NAME="VPN_firstvds.ru"
+NET_CONNECTION_NAME="vpn_net"
 
-sleep 30
+sleep $START_TIMEOUT
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 echo "$dt starting script" >> testVPN.log
 while true
@@ -10,7 +14,8 @@ do
 	#if ping -c 1 ${SERVER_IP_ADDR} &> /dev/null
 	#ping -c 1 ${SERVER_IP_ADDR} &> /dev/null
 	pingResult=$(ping -c 1 ${SERVER_IP_ADDR})
-	if [ "$pingResult" = 0 ]
+	#if [ "$pingResult" = 0 ]
+	if [ "$?" = 0 ]
 		then
 			 echo "$dt server found"
 			 echo "$dt server found" >> testVPN.log
@@ -23,10 +28,14 @@ do
 			 echo "ping result:" >> testVPN.log
 			 echo $pingResult >> testVPN.log
 			 echo "$dt server not found! Starting VPN!" >> testVPN.log
-			 nmcli con down ${VPN_CONNECTION_NAME}
+			 nmcli con down id ${NET_CONNECTION_NAME}
+			 sleep 5
+			 nmcli con up id ${NET_CONNECTION_NAME}
+			sleep 15
+			 nmcli con down id ${VPN_CONNECTION_NAME}
 			 sleep 2
-			 nmcli con up ${VPN_CONNECTION_NAME}
+			 nmcli con up id ${VPN_CONNECTION_NAME}
 			 echo "$dt Ended starting VPN!!" >> testVPN.log
 	fi
-sleep 20
+sleep $CHECK_TIMEOUT
 done
